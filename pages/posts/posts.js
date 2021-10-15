3e98b4d3d7d8db63d258ba117c83b023fc6cada5
CrazySpark <644873708@qq.com>
2021-10-15 15:11:34 +0800

    学习for

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -17,19 +17,31 @@ Page({
      */
     onLoad: function (options) {
         var content = [{
-                date: "2021",
+                date: "2021-10-02",
+                title:"S2赛季",
                 name: "hello",
-                age: 123
+                age: 123,
+                collect:323,
+                review:32134,
+                image:"/images/avatar/banner1.png"
             }, {
 
-                date: "2024",
+                date: "2024-10-02",
+                title:"S232赛季",
                 name: "web",
-                age: 122325
+                age: 122325,
+                collect:22,
+                review:321,
+                image:"/images/avatar/banner2.png"
             },
             {
-                date: "2026",
+                date: "2026-10-02",
+                title:"S12赛季",
                 name: "world",
-                age: 553
+                age: 553,
+                collect:78,
+                review:4588,
+                image:"/images/avatar/panghu2.jpg"
             }
         ]
         this.setData({
